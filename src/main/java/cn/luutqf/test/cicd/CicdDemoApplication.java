6f5aeca79088b1f71db9a4874c30bea31e8df393
luutqf <luutqf@gmail.com>
2019-02-15 20:45:14 +0800

    create

---
+++ src/main/java/cn/luutqf/test/cicd/CicdDemoApplication.java
@@ -0,0 +1,21 @@
+package cn.luutqf.test.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdDemoApplication.class, args);
+    }
+
+    @GetMapping
+    public Object get(){
+        return "2019年2月15日20:33:41";
+    }
+}
+
